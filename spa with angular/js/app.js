a40220b7216520571bfc639600f54e8cd7ec3b36
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2023-09-14 18:16:33 -0300

    upgrade(app.js | index.html) - adicionando botao

--- spa with angular/js/app.js
+++ spa with angular/js/app.js
@@ -1,4 +1,12 @@
-angular.module('expensesApp', [])
-    .controller('ExpensesViewController', ['$scope', function ($scope) {
-        $scope.name = "Nome de Teste";
-    }]);
+let app = angular.module('expensesApp', []);
+app.controller('ExpensesViewController', ['$scope', function ($scope) {
+    $scope.name = "Nome de Teste";
+    $scope.phrase = "The Sky is blue";
+    $scope.expense = {
+        description: 'food',
+        amount: 10
+    }
+    $scope.increaseAmount = function () {
+        $scope.expense.amount++;
+    }
+}]);
